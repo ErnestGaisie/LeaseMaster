469ce295f95b21cec13a3235adda5d0dad7173fd
efuaaa <plangette19@gmail.com>
2019-10-21 08:48:30 +0000

    Initial Commit

---
+++ LeaseMaster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LeaseMaster
+//
+//  Created by user on 21/10/2019.
+//  Copyright © 2019 Qodehub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
