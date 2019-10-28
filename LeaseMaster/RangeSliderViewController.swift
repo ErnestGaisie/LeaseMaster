df6de8b5b7f419eeea1a232bacf5178b476cf0a4
efuaaa <plangette19@gmail.com>
2019-10-28 14:01:21 +0000

    Currently working on Price range slider

---
+++ LeaseMaster/RangeSliderViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RangeSliderViewController.swift
+//  LeaseMaster
+//
+//  Created by user on 28/10/2019.
+//  Copyright © 2019 Qodehub. All rights reserved.
+//
+
+import UIKit
+
+class RangeSliderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
