3b5727a02480c96e6d7d1b22a0e519ebc3ef950f
efuaaa <plangette19@gmail.com>
2019-10-29 09:20:14 +0000

    Range Slider completed

--- LeaseMaster/RangeSliderViewController.swift
+++ LeaseMaster/RangeSliderViewController.swift
@@ -10,21 +10,60 @@ import UIKit
 
 class RangeSliderViewController: UIViewController {
 
+    @IBOutlet weak var rangeSlider: RangeSeekSlider!
+    @IBOutlet weak var minPrice: UILabel!
+    @IBOutlet weak var maxPrice: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+           rangeSlider.delegate = self
+       setupLabels(label:minPrice)
+         setupLabels(label:maxPrice)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+   func setupLabels(label:UILabel){
+        label.layer.cornerRadius = 4.0
+        label.layer.borderWidth = 1.0
+        label.layer.borderColor = UIColor.veryLightPink.cgColor
+        
+        
+//         // Create a padding view for padding on left
+//        label.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: label.frame.height))
+//        label.leftViewMode = .always
+//               
+//        // Create a padding view for padding on right
+//        label.rightView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: label.frame.height))
+//        label.rightViewMode = .always
+       
     }
-    */
+}
 
+
+
+//// MARK: - RangeSeekSliderDelegate
+//
+extension RangeSliderViewController: RangeSeekSliderDelegate {
+
+    func rangeSeekSlider(_ slider: RangeSeekSlider, didChange minValue: CGFloat, maxValue: CGFloat) {
+        if slider === rangeSlider {
+            print("Standard slider updated. Min Value: \(minValue) Max Value: \(maxValue)")
+            let a = Int(minValue)
+            let b = Int(maxValue)
+            minPrice.text = "¢ " + "\(a)"
+              maxPrice.text = "¢ " + "\(b)"
+        }
+        else{
+            print("blahjhhhh")
+        }
+    }
+
+    func didStartTouches(in slider: RangeSeekSlider) {
+        print("did start touches")
+    }
+
+    func didEndTouches(in slider: RangeSeekSlider) {
+        print("did end touches")
+    }
 }
